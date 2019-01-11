39494a0521ca575ca8afa6ea36e23509c9f34e28
Vince Tang <vince.tang@thomsonreuters.com>
2019-01-11 09:38:34 -0500

    Added some documentation and clean up

--- AutomatedCollection.py
+++ AutomatedCollection.py
@@ -12,7 +12,6 @@ from shutil import copyfile, rmtree
 TARGET_EXTENSIONS = []
 TARGET_REGEX_PATTERNS = []
 MATCH_IGNORE_CASE = False
-COLLECTION_LOCATION = ''
 MD5 = hashlib.md5()
 
 OWNER_RWX_PERMISSIONS = stat.S_IRWXU
@@ -29,28 +28,35 @@ logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
 
-def execute_automated_collection(target_dir, archive, archive_name, upload, container, clean_up):
+def execute_automated_collection(src, dest, archive, archive_name, upload, container, clean_dest):
     """
-    Traverses all files and folders from path, looking for files with an extension that matches one in EXTENSIONS.
-    Read and execute permissions for directories will be temporarily granted to the user to continue traversal.
-    Read permissions for files will be temporarily granted to copy files.
-    :param path: An absolute path in the filesystem (optional)
+    Traverses recursively through the src directory and copies any files that match at least one pattern in
+     TARGET_REGEX_PATTERNS or with an extension listed in TARGET_EXTENSIONS. Files are copied into the dest directory.
+     If archive is True, a tarball will be created with the name archive_name in the dest directory. If clean_dest is
+     True, all collected documents will be deleted at the end of the script.
+    :param src: Path to directory to traverse
+    :param dest: Path of directory to save files in
+    :param archive: If True, dest will be tarballed
+    :param archive_name: The name of the tarball
+    :param upload: If True, files will be uploaded to docker container. Only the tarball will be uploaded if archived.
+    :param container: The name of the container to copy files to
+    :param clean_dest: If True, deletes all collected files at the end of the script
     :return:
     """
     pause_bash_history_command = "set +o history"
     run_bash_command(pause_bash_history_command)
 
-    create_and_validate_dir(COLLECTION_LOCATION)
-    search_filesystem(target_dir)
+    create_and_validate_permissions(dest)
+    search_filesystem(src, dest)
 
     if archive:
-        archive_files(COLLECTION_LOCATION, os.path.join(COLLECTION_LOCATION, archive_name))
+        archive_files(dest, os.path.join(dest, archive_name))
 
     if upload:
         print("uploading")
 
-    if clean_up:
-        clean_collection_location()
+    if clean_dest:
+        clean_files(dest)
 
     resume_bash_history_command = "set -o history"
     run_bash_command(resume_bash_history_command)
@@ -58,12 +64,12 @@ def execute_automated_collection(target_dir, archive, archive_name, upload, cont
 
 def run_bash_command(command):
     process = subprocess.Popen(command.split(), shell=True, stdout=subprocess.DEVNULL)
-    output, error = process.communicate()
+    process.communicate()
 
 
-def search_filesystem(path):
-    for root, dirs, files in os.walk(path):
-        # Check for directories that can't be accessed
+def search_filesystem(src, dest):
+    for root, dirs, files in os.walk(src):
+
         for dir in dirs:
             d = os.path.join(root, dir)
             original_permissions = get_permissions(d)
@@ -92,7 +98,7 @@ def search_filesystem(path):
                 name, ext = os.path.splitext(file)
                 unique_filename = ''.join([name, '-', MD5.hexdigest(), ext])
 
-                copyfile(f, os.path.join(COLLECTION_LOCATION, unique_filename))
+                copyfile(f, os.path.join(dest, unique_filename))
 
 
 def file_matches_target_extension(f):
@@ -108,7 +114,7 @@ def collect_file(src, dest):
     copyfile(src, dest)
 
 
-def create_and_validate_dir(d):
+def create_and_validate_permissions(d):
     if not os.path.exists(d):
         os.makedirs(d)
     modify_permissions(d, OWNER_RWX_PERMISSIONS)
@@ -132,25 +138,31 @@ def upload_collections(container, src, dest):
     run_bash_command(docker_upload_command)
 
 
-def clean_collection_location():
-    rmtree(COLLECTION_LOCATION)
+def clean_files(dest):
+    rmtree(dest)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Automated Collection.')
-    parser.add_argument('--target_dir', type=str, default='/', help='An absolute path to the directory to begin traversing in')
+    parser.add_argument('--src', type=str, default='/', help='An absolute path to the directory to begin traversing in')
+    parser.add_argument('--dest', type=str, default='/collect', help='An absolute path to the directory files will '
+                                                                     'be collected in')
     parser.add_argument('--target_extensions', nargs='*', help='A list of one or more extensions of files to collect')
-    parser.add_argument('--target_regex', nargs='*', help='A list of one or more regex patterns. Files matching any of these patterns will be collected')
-    parser.add_argument('--archive', type=bool, default=True, help='Creates a tar ball after all the files have been collected')
-    parser.add_argument('--archive_name', type=str, default='collection.tar.gz', help='The name of the archive if created')
-    parser.add_argument('--upload', type=bool, default=False, help='Uploads collection to a docker container. A container name must be provided with the --container_name argument')
+    parser.add_argument('--target_regex', nargs='*', help='A list of one or more regex patterns. Files matching any of '
+                                                          'these patterns will be collected')
+    parser.add_argument('--archive', type=bool, default=True, help='Creates a tar ball after all the files have been '
+                                                                   'collected')
+    parser.add_argument('--archive_name', type=str, default='collection.tar.gz', help='The name of the archive if '
+                                                                                      'created')
+    parser.add_argument('--upload', type=bool, default=False, help='Uploads collection to a docker container. '
+                                                                   'A container name must be provided with '
+                                                                   'the --container_name argument')
     parser.add_argument('--container_name', type=str, help='The name of the docker container to upload files to')
     parser.add_argument('--clean_up', type=bool, default=False, help='Removes all files saved to the collection')
     args = parser.parse_args()
     TARGET_EXTENSIONS = args.target_extensions
-    TARGET_REGEX_PATTERNS = args.target_regex
+    TARGET_REGEX_PATTERNS = [re.compile(p) for p in args.target_regex]
     if TARGET_EXTENSIONS is None and TARGET_REGEX_PATTERNS is None:
         print("At least one extension or regex pattern must be provided")
     else:
-        COLLECTION_LOCATION = '/Users/vince/Desktop/collection'
-        execute_automated_collection(args.target_dir, args.archive, args.archive_name, args.upload, args.container_name, args.clean_up)
+        execute_automated_collection(args.src, args.dest, args.archive, args.archive_name, args.upload, args.container_name, args.clean_up)
