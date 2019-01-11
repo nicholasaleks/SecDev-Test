e01ab75f099afb19df94a18ea2523aca9f10dd31
Vince Tang <vince.tang@thomsonreuters.com>
2019-01-11 07:26:09 -0500

    Wrote out skeleton of approach

--- AutomatedCollection.py
+++ AutomatedCollection.py
@@ -1,2 +1,27 @@
-if __name__ == '__main__':
+
+EXTENSIONS = []
+
+def get_file_permissions(f):
     pass
+
+def modify_file_permissions(f, p):
+    pass
+
+def copy_file(src, dest):
+    # Look out for:
+    #    - name collisions
+    #    - permissions
+    pass
+
+def execute_automated_collection():
+    # Traverse file system and look for files that match extension
+    # Look out for:
+    #       - File permissions (both to search and copy)
+    #       - case sensitivity
+    #       - complex nested fs
+    #       - performance of search
+    # Copy matching files to some location
+    pass
+
+if __name__ == '__main__':
+    execute_automated_collection()
