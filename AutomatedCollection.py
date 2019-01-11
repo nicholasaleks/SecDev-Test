9b3a2dd65a09b7892f7775cd79246ced58e85a46
Vince Tang <vince.tang@thomsonreuters.com>
2019-01-11 07:40:01 -0500

    Added code to begin traversing filesystem

--- AutomatedCollection.py
+++ AutomatedCollection.py
@@ -1,3 +1,4 @@
+import os
 
 EXTENSIONS = []
 
@@ -13,7 +14,14 @@ def copy_file(src, dest):
     #    - permissions
     pass
 
-def execute_automated_collection():
+def execute_automated_collection(path='/'):
+    """
+    Traverses all files and folders from path, looking for files with an extension that matches one in EXTENSIONS.
+    Read and execute permissions for directories will be temporarily granted to the user to continue traversal.
+    Read permissions for files will be temporarily granted to copy files.
+    :param path: An absolute path in the filesystem (optional)
+    :return:
+    """
     # Traverse file system and look for files that match extension
     # Look out for:
     #       - File permissions (both to search and copy)
@@ -22,6 +30,13 @@ def execute_automated_collection():
     #       - performance of search
     # Copy matching files to some location
     pass
+    for root, dirs, files in os.walk(path):
+        for dir in dirs:
+            d = os.path.join(root, dir)
+            print(d)
+        for file in files:
+            f = os.path.join(root, file)
+            print(f)
 
 if __name__ == '__main__':
-    execute_automated_collection()
+    execute_automated_collection("/Users/vince/Desktop/test")
